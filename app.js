ce82910184eecb01430e26b71d0d2d06e9725660
parksungho <parksungho86@gmail.com>
2014-11-26 17:03:36 +0900

    s

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var express = require('express')
 server.listen(8000);
 require('./chat.js').setIo(io).setMaker('AnimalChess');
 
-var express = require('express')
-	, app = express()
-	, http = require('http')
-	, server = http.createServer(app)
-	, io = require('socket.io').listen(server);
-server.listen(8001);
-require('./chat.js').setIo(io).setMaker('BlacknWhite');
+var express2 = require('express')
+	, app2 = express2()
+	, http2 = require('http')
+	, server2 = http2.createServer(app2)
+	, io2 = require('socket.io').listen(server2);
+server2.listen(8001);
+require('./chat.js').setIo(io2).setMaker('BlacknWhite');
 
 
 
